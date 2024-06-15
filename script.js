ca26ea9d7f08fdae08a8a916d189c27bb1a7e36e
pefjsjxj <darksn29@gmail.com>
2024-06-15 09:35:17 -0500

    e

--- script.js
+++ script.js
@@ -28,7 +28,6 @@ function dividir() {
 
 function potencia() {
   var num1 = parseFloat(document.getElementById('num1').value);
-  var num2 = parseFloat(document.getElementById('num2').value);
   var resultado = num1 ^ num2;
   document.getElementById('resultado').innerText = resultado;
 }
